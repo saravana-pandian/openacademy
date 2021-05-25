8ec3a21c5a43a727cc56be598e5dd8f1e148a1d2
saravana-pandian <saravanaoff2000@gmail.com>
2021-05-25 17:26:37 +0530

    new one

---
+++ models/models.py
@@ -0,0 +1,9 @@
+from odoo import models, fields, api
+
+class course(models.Model):
+	_name = 'openacademy.course'
+	_description = "Openacademy Course"
+
+	name = fields.Char(string="Title", required=True)
+	description = fields.Text()
+	
