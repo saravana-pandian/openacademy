458173b9a8c78e729a78ea30145c878a7f15d19a
saravana-pandian <saravanaoff2000@gmail.com>
2021-05-31 13:17:56 +0530

    commited 3

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 {
     'name': "openacademy",
-
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
@@ -9,29 +8,21 @@
     'description': """
         this open academy module is for odoo technical training
     """,
-
     'author': "__s_a_r_v_a_n_a___",
     'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+    'sequence': "-1000",
     'category': 'Uncategorized',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base'],
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/security.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'views/openacademy.xml',
         'views/session.xml',
         'views/partner.xml'
     ],
-    # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
